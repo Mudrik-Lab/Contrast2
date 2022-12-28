f35f55283443c2efae6b069b314de534ba717ab6
alonisser <alonisser@gmail.com>
2022-12-28 17:55:05 +0200

    progress with profile model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
+from users.models import Profile
+
 
 # Register your models here.
+
+
+class ProfileAdmin(ImportExportModelAdmin):
+    fields = ("user",)
+
+
+admin.site.register(Profile, ProfileAdmin)

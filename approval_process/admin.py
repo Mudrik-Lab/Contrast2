95f7e5ef1c9b8b3c3624557cec584a1347ea4b16
alonisser <alonisser@gmail.com>
2022-12-30 22:10:14 +0200

    todo

--- approval_process/admin.py
+++ approval_process/admin.py
@@ -13,12 +13,15 @@ class ApprovalCommentInline(admin.TabularInline):
 
 class ApprovalProcessAdmin(ImportExportModelAdmin):
     fields = ("started_at", "status", "study")
-    list_filter = ("status",)
+    list_filter = ("status",)  # TODO add filter for "stuck" in process approvals by date
+    # TODO add drop down filters for reviewers
 
     inlines = [
         ApprovalCommentInline
     ]
-    #TODO: custom action for sending to reviewers
+    # TODO: custom action for sending to reviewers
 
+
+# TODO: find out how to show this admin to reviewers without the rest
 
 admin.site.register(ApprovalProcess, ApprovalProcessAdmin)

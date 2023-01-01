1dd73866f625002a7a840c5897e12becef2952e4
alonisser <alonisser@gmail.com>
2023-01-01 08:47:20 +0200

    place holder for tests

--- studies/tests/test_experiments_endpoints.py
+++ studies/tests/test_experiments_endpoints.py
@@ -16,3 +16,32 @@ class ExperimentsViewSetTestCase(APITestCase):
         target_url = reverse("experiments-list")
         res = self.client.get(target_url)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+    def test_nations_of_consciousness_data(self):
+        """
+        provide graph_type=nations_of_consciousness, min_experiments?=int
+        returns experiments grouped by country and interpretations (positive to theory)
+        """
+        pass
+
+    def test_publications_by_theory_family_data(self):
+        """
+        provide graph_type=publications_by_theory, min_experiments?=int, theory=GNW
+        group by publications names
+        """
+        pass
+
+    def test_frequencies_graph(self):
+        pass
+
+
+    def test_timings_graphs(self):
+        pass
+
+    def test_theory_driven_by_interpretations(self):
+        """
+        is_reporting[]=true, TypeOfConsciousness[]=state
+        hint: use request.query_params.getlist
+        """
+        pass

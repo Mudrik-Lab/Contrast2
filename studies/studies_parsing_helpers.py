0fb1cb47e8cc83c9bd98d852ab0b3dc7c7fcc136
dani-efr <69506413+dani-efr@users.noreply.github.com>
2023-01-08 22:24:19 +0200

    added parsing helpers, fixed test for parsers

--- studies/studies_parsing_helpers.py
+++ studies/studies_parsing_helpers.py
@@ -1,6 +1,16 @@
 def parse_authors_from_affiliation_text(text:str):
-    pass
+    authors_list = text.split(", ")
+
+    return authors_list
 
 
 def resolve_country_from_affiliation_text(text:str):
-    pass
+    affiliations_list = text.split("; ")
+    countries = []
+    for affiliation in affiliations_list:
+        country = affiliation.split(", ")[-1]
+        countries.append(country)
+
+    set_of_countries = set(countries)
+
+    return set_of_countries

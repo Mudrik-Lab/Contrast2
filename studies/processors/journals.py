592de0272d54e9ea76a9c885a88c7cd2e21d4a2d
alonisser <alonisser@gmail.com>
2023-01-19 09:51:44 +0200

    standarize processors

--- studies/processors/journals.py
+++ studies/processors/journals.py
@@ -1,12 +1,33 @@
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Count, F
 
-from studies.models import Experiment
+from studies.choices import InterpretationsChoices
+from studies.models import Experiment, Interpretation
 from studies.processors.base import BaseProcessor
 
 
 class JournalsGraphDataProcessor(BaseProcessor):
     def __init__(self, experiments: QuerySet[Experiment], **kwargs):
+        self.theory = kwargs.pop("theory")
+
         super().__init__(experiments=experiments, **kwargs)
 
     def process(self):
-        pass
+        relevant_experiments = self.get_queryset()
+
+        aggregate = self.aggregate(relevant_experiments)
+
+        return aggregate
+
+    def get_queryset(self):
+        experiments_by_theory = Interpretation.objects \
+            .filter(type=InterpretationsChoices.PRO,
+                    theory__parent__name=self.theory,
+                    experiment__in=self.experiments) \
+            .select_related("experiment", "experiment__study") \
+            .values("experiment", "experiment__study") \
+            .annotate(journal=F("experiment__study__abbreviated_source_title"))
+        return experiments_by_theory
+
+    def aggregate(self, qs):
+        # having "values" before annotate with count results in a "select *, count(1) from .. GROUP BY
+        return qs.values("journal").annotate(count=Count("id")).order_by("-count", "journal")

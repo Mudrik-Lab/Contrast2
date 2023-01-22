fe61d7d02dc5d2abbfc9ba5f640ed379a90124cb
alonisser <alonisser@gmail.com>
2023-01-22 11:54:33 +0200

    adding more fields on profile

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.conf import settings
+from django_countries.fields import CountryField
 
 from users.choices import AcademicStageChoices, GenderChoices
 
@@ -11,9 +12,11 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
     date_of_birth = models.DateField(null=True, blank=True, validators=[MinValueValidator(datetime.date(1900, 1, 1)),
                                                                         MaxValueValidator(datetime.date(2100, 1, 1))])
-    gender = models.CharField(max_length=50, null=True, blank=True, choices=GenderChoices.choices)
+    self_identified_gender = models.CharField(max_length=50, null=True, blank=True, choices=GenderChoices.choices)
     academic_affiliation = models.TextField(null=True, blank=True)
+    country_of_residence = CountryField(null=True, blank=True)
     academic_stage = models.CharField(max_length=50, null=True, blank=True, choices=AcademicStageChoices.choices)
+    has_ASSC_membership = models.BooleanField(null=True, blank=True)
 
     @classmethod
     def create_profile(cls, user):

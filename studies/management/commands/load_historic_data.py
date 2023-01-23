21bcd1c0316e0de8c8b6747b9ee970dc4ad8a04d
dani-efr <69506413+dani-efr@users.noreply.github.com>
2023-01-23 16:53:08 +0200

    added excel to JSON conversion in load historic data command

--- studies/management/commands/load_historic_data.py
+++ studies/management/commands/load_historic_data.py
@@ -1,9 +1,17 @@
 from django.core.management import BaseCommand
-
+import pandas
+import json
 
 class Command(BaseCommand):
     help = 'Load historic data'
 
     def handle(self, *args, **options):
-        print("hey there")
 
+        # Read Excel document and convert to JSON-file
+        excel_data_df = pandas.read_excel('studies/data/Contrast2_Data_For_Drorsoft.xlsx', sheet_name='sheet1',
+                                          index_col=2)
+        historic_data_json = excel_data_df.to_json(orient='records')
+        historic_data_json_dict = json.loads(historic_data_json)
+
+        with open('historic_data.json', 'w') as json_file:
+            json.dump(historic_data_json_dict, json_file)

cd93c7cd92635aadef5f336696e1ed50725c8027
alonisser <alonisser@gmail.com>
2023-02-03 23:53:48 +0200

    build initial serializers

--- studies/serializers.py
+++ studies/serializers.py
@@ -1,16 +1,138 @@
 from rest_framework import serializers
 
-from studies.models import Experiment, Study
+from studies.models import Experiment, Study, Theory, Interpretation, Technique, Author, Paradigm, ConsciousnessMeasure, \
+    Measure, FindingTag, Sample, Stimulus, Task
+
+
+class TheorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Theory
+        depth = 2
+        fields = ('id', 'name', 'parent')
+
+
+class MeasureSerializer(serializers.ModelSerializer):
+    type = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+    class Meta:
+        model = Measure
+        fields = ("id", "type", "notes")
+
+
+class FindingTagSerializer(serializers.ModelSerializer):
+    type = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    family = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    technique = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+    class Meta:
+        model = FindingTag
+        fields = ("id",
+                  "family",
+                  "type",
+                  "onset",
+                  "offset",
+                  "band_lower_bound",
+                  "band_higher_bound",
+                  "AAL_atlas_tag",
+                  "notes",
+                  "analysis_type",
+                  "technique")
+
+
+class InterpretationSerializer(serializers.ModelSerializer):
+    theory = TheorySerializer()
+
+    class Meta:
+        model = Interpretation
+        fields = ("theory", "type")
+
+
+class ConsciousnessMeasureSerializer(serializers.ModelSerializer):
+    phase = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    type = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+    class Meta:
+        model = ConsciousnessMeasure
+        fields = ("id", "phase", "type", "description")
+
+
+class ParadigmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paradigm
+        depth = 2
+        fields = ("id", "name", "parent")
+
+
+class SampleSerializer(serializers.ModelSerializer):
+    type = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+    class Meta:
+        model = Sample
+        fields = ("id", "type", "total_size", "size_included")
+
+
+class StimulusSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    sub_category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    modality = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+    class Meta:
+        model = Stimulus
+        fields = ("id", "category", "sub_category", "modality", "description", "duration")
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    task = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+    class Meta:
+        model = Task
+        fields = ("id", "description", "type")
 
 
 class ExperimentSerializer(serializers.ModelSerializer):
+    interpretations = InterpretationSerializer(many=True)
+    finding_tags = FindingTagSerializer(many=True, source="finding_tags")
+    measures = MeasureSerializer(many=True, source="measures")
+    samples = SampleSerializer(many=True, source="samples")
+    stimuli = StimulusSerializer(many=True, source="stimuli")
+    tasks = TaskSerializer(many=True, source="tasks")
+    consciousness_measures = ConsciousnessMeasureSerializer(many=True, source="consciousness_measures")
+    techniques = serializers.SlugRelatedField(many=True, slug_field="name", read_only=True)
+    paradigms = serializers.SlugRelatedField(many=True, slug_field="name", read_only=True)
+    theory_driven_theories = serializers.SlugRelatedField(many=True, slug_field="name", read_only=True)
+
     class Meta:
         model = Experiment
         depth = 2
-        fields = "__all__"
+        fields = ("id",
+                  "study",
+                  "interpretations",
+                  "finding_description",
+                  "techniques",
+                  "paradigms",
+                  "type_of_consciousness",
+                  "is_reporting",
+                  "theory_driven",
+                  "theory_driven_theories",
+                  "type",
+                  "finding_tags",
+                  "measures",
+                  "consciousness_measures",
+                  "samples",
+                  "stimuli",
+                  "tasks"
+                  )
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ["id", "name"]
 
 
 class StudySerializer(serializers.ModelSerializer):
+    authors = AuthorSerializer(many=True)
+
     class Meta:
         model = Study
         fields = ["authors",
@@ -24,7 +146,8 @@ class StudySerializer(serializers.ModelSerializer):
                   "source_title",
                   "abbreviated_source_title",
                   "countries",
-                  "affiliations"
+                  "affiliations",
+                  "submitter"
                   ]
 
 

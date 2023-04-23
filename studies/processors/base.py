fa895d7526531d2880b46b220acb81dae410fe8a
alonisser <alonisser@gmail.com>
2023-04-23 23:33:04 +0300

    fix min experiments

--- studies/processors/base.py
+++ studies/processors/base.py
@@ -6,7 +6,7 @@ from studies.models import Experiment
 class BaseProcessor:
     def __init__(self, experiments: QuerySet[Experiment], **kwargs):
         self.experiments: QuerySet[Experiment] = experiments
-        min_number_of_experiments = kwargs.pop("min_number_of_experiments", 0)
+        min_number_of_experiments = int(kwargs.pop("min_number_of_experiments", ["0"])[0])
         if min_number_of_experiments > 0:
             # so everything that explicitly sets a minimum it would be supported
             min_number_of_experiments = min_number_of_experiments - 1

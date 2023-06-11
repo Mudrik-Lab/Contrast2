8c0fbd2aabdaded6d0d9eb40b6bfa314029074dc
alonisser <alonisser@gmail.com>
2023-06-11 21:39:57 +0300

    WIP profiles endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import mixins
+
+from studies.permissions import SelfOnlyProfilePermission
+from users.models import Profile
+from users.serializers import ProfileSerializer
+
 
 # Create your views here.
+
+class ProfilesView(GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin):
+    queryset = Profile.objects.all()
+    permission_classes = (SelfOnlyProfilePermission, )
+
+    @action(detail=True, methods=["POST"], serializer_class=ProfileSerializer,
+            permission_classes=[AllowAny])
+    def register(self, request, **kwargs):
+        pass

c9894a118234a12a248d74625fd398ad75ceeac0
alonisser <alonisser@gmail.com>
2023-06-22 21:30:38 +0300

    html templates

--- users/views/feedback.py
+++ users/views/feedback.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from users.serializers import SuggestNewQuerySerializer, ContactUsSerializer, VetAPaperSerializer, FeedbackSerializer
@@ -8,17 +10,18 @@ class FeedbackView(GenericViewSet):
 
     @action(methods=["POST"], detail=False, serializer_class=ContactUsSerializer)
     def contact_us(self, request, *args, **kwargs):
-        pass
+        return Response(data=dict(submitted=True), status=status.HTTP_200_OK)
 
     @action(methods=["POST"], detail=False, serializer_class=SuggestNewQuerySerializer)
     def suggest_new_query(self, request, *args, **kwargs):
-        pass
+        return Response(data=dict(submitted=True), status=status.HTTP_200_OK)
 
     @action(methods=["POST"], detail=False, serializer_class=VetAPaperSerializer)
     def vet_a_paper(self, request, *args, **kwargs):
-        pass
+        return Response(data=dict(submitted=True), status=status.HTTP_200_OK)
 
     @action(methods=["POST"], detail=False, serializer_class=FeedbackSerializer)
     def feedback(self, request, *args, **kwargs):
-        pass
+        return Response(data=dict(submitted=True), status=status.HTTP_200_OK)
+
 

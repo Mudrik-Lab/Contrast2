8655e8f733100977bdb82d46a1d2b5125722ed44
alonisser <alonisser@gmail.com>
2023-12-20 09:20:47 +0200

    unique on migrations

--- configuration/models.py
+++ configuration/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class GraphImage(models.Model):
-    key = models.CharField(max_length=50, null=False, blank=False)
+    key = models.CharField(max_length=50, null=False, blank=False, unique=True)
     image = models.FileField(upload_to="graph_images/")

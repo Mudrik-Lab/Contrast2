c2e8c31f205c72be9c9dce138cd2f4c665658af7
dani-efr <69506413+dani-efr@users.noreply.github.com>
2024-04-04 22:38:39 +0300

    create migration for initial data types

---
+++ configuration/uncontrast_initial_data_types.py
@@ -0,0 +1,196 @@
+main_paradigms = [
+    "Priming",
+    "Emergence to awareness",
+    "Attention allocation",
+    "Change Blindness",
+    "Learning",
+    "Cognitive Tasks",
+    "Computational modeling",
+    "Illusions",
+    "Sperling Like",
+    "Visual search"
+]
+
+specific_paradigms = {
+    "Priming": [
+        "Semantic",
+        "Affective",
+        "Perceptual",
+        "Action",
+    ],
+    "Emergence to awareness": [
+        "B-CFS",
+        "Mooney images"
+    ],
+    "Learning": [
+        "Instrumental conditioning",
+        "Evaluative conditioning",
+        "Eyeblink conditioning",
+        "Conflict adaptation",
+        "Associative learning"
+    ],
+    "Cognitive Tasks": [
+        "Executive control",
+        "Working memory",
+        "Task switching",
+        "Memory",
+        "Language"
+    ]
+}
+
+tasks = [
+    "Numerical categorization",
+    "Lexical categorization",
+    "Semantic categorization",
+    "Perceptual categorization",
+    "Go/No go",
+    "Search",
+    "Identification",
+    "Eye fixation task",
+    "Temporal order judgment",
+    "Free viewing",
+    "Discrimination",
+    "Detection",
+    "Task is irrelevant to the effect"
+]
+
+stimulus_categories = [
+    "Lingual",
+    "Numerical",
+    "Pictures",
+    "Shapes",
+    "Line Drawings",
+    "Tactile Stimuli"
+]
+
+stimulus_sub_categories = {
+    "Lingual": [
+        "Words",
+        "Letters",
+        "Sentences"
+    ],
+    "Numerical": [
+        "Digits",
+        "Numbers",
+        "Mathematical exercise"
+    ],
+    "Pictures": [
+        "Faces",
+        "Objects",
+        "Scenes",
+        "Animals",
+        "Vehicles",
+        "Logos",
+        "Hands",
+        "Other"
+    ],
+    "Shapes": [
+        "Arrows",
+        "Gratings",
+        "Simple shapes",
+        "Other"
+    ],
+    "Line Drawings": [
+        "Faces",
+        "Objects",
+        "Scenes",
+        "Animals",
+        "Vehicles",
+        "Logos",
+        "Hands",
+        "Other"
+    ]
+}
+
+stimulus_modalities = [
+    "Auditory",
+    "Olfactory",
+    "Tactile",
+    "Visual",
+    "None"
+]
+
+main_suppression_methods = [
+    "Masking",
+    "Dichoptic masking",
+    "CFS",
+    "Crowding",
+    "Sub-threshold stimuli",
+    "Attentional Blink",
+    "Parafoveal display",
+    "Stimulus degradation",
+    "Inattentional blindness",
+    "Binocular rivalry",
+    "Change blindness",
+    "Flickering",
+    "Dichotic fusion"
+]
+
+specific_suppression_methods = {
+    "Masking": [
+        "Backward target Masking",
+        "Backward pattern masking",
+        "Forward masking",
+        "Metacontrast masking",
+        "Object Substitution masking",
+        "Paracontrast masking",
+        "Sandwich target masking",
+        "Sandwich pattern masking"
+    ],
+    "Sub-threshold stimuli": "Mooney Images"
+}
+
+processing_domains = [
+    "Lingual",
+    "Visual discrimination",
+    "Numerical",
+    "Memory",
+    "Action priming",
+    "Executive functions",
+    "Working memory",
+    "Attention allocation"
+]
+
+consciousness_measures_main_types = [
+    "Objective",
+    "Subjective",
+    "Verbal Debriefing",
+    "None",
+    "Other"
+]
+
+consciousness_measures_specific_types = {
+    "Objective": [
+        "High-level discrimination",
+        "Low-level discrimination",
+        "Location",
+        "Detection (presence/absence)",
+        "Other"
+    ],
+    "Subjective": [
+        "Dichotomous",
+        "Perception Awareness Scale (PAS)",
+        "Post-decision wagering",
+        "Confidence ratings",
+        "Continuous scale",
+        "Likert Scale",
+        "Other"
+    ]
+}
+
+consciousness_measures_phases = [
+    "Post-experiment",
+    "Pre-experiment",
+    "Separate sample",
+    "During the experiment, in alternating blocks",
+    "Trial-by-trial",
+    "None"
+]
+
+finding_outcomes = [
+    "Reaction times",
+    "Accuracy",
+    "Eye movements",
+    "Other"
+]
+

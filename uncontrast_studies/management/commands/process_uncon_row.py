40651a949c9eb20baef81610c634e62567214128
dani-efr <69506413+dani-efr@users.noreply.github.com>
2024-05-03 13:39:26 +0300

    Update process_uncon_row.py

--- uncontrast_studies/management/commands/process_uncon_row.py
+++ uncontrast_studies/management/commands/process_uncon_row.py
@@ -7,10 +7,14 @@ from contrast_api.data_migration_functionality.studies_parsing_helpers import Pr
 from studies.models import Study
 from uncontrast_studies.models import UnConExperiment, UnConSpecificParadigm, UnConTask, UnConSuppressedStimulus, \
     UnConTargetStimulus, UnConSuppressionMethodType, UnConSuppressionMethodSubType, UnConSuppressionMethod, UnConSample, \
-    UnConProcessingMainDomain, UnConProcessingDomain, UnConFinding
-from uncontrast_studies.parsers.uncon_data_parsers import resolve_uncon_paradigm, resolve_uncon_stimuli_metadata, \
-    resolve_uncon_task, resolve_uncon_stimuli, resolve_uncon_suppression_method, resolve_uncon_samples, \
-    resolve_uncon_findings, resolve_uncon_processing_domains
+    UnConProcessingMainDomain, UnConProcessingDomain, UnConFinding, UnConsciousnessMeasurePhase, \
+    UnConsciousnessMeasureType, UnConsciousnessMeasureSubType, UnConsciousnessMeasure
+from uncontrast_studies.parsers.consciousness_measure_parser import resolve_consciousness_measures
+from uncontrast_studies.parsers.uncon_data_parsers import resolve_uncon_paradigm, resolve_uncon_task, resolve_uncon_processing_domains
+from uncontrast_studies.parsers.stimulus_parser import resolve_uncon_stimuli, resolve_uncon_stimuli_metadata
+from uncontrast_studies.parsers.suppression_method_parser import resolve_uncon_suppression_method
+from uncontrast_studies.parsers.sample_parser import resolve_uncon_samples
+from uncontrast_studies.parsers.finding_parser import resolve_uncon_findings
 
 ExperimentDuplicate = namedtuple("ExperimentDuplicateTuple", ["key", "id"])
 
@@ -94,9 +98,26 @@ def process_uncon_row(item: dict, duplicated_experiments: dict):
     for finding in finding_data:
         UnConFinding.objects.create(experiment=experiment, outcome=finding.outcome, is_significant=finding.is_significant,
                                     is_important=finding.is_important, number_of_trials=finding.number_of_trials)
+        if finding.notes:
+            experiment.experiment_findings_notes.add(f"{finding.notes}; ")
 
     # consciousness measures
-
-
+    consciousness_measures_data = resolve_consciousness_measures(item=item, index=composit_experiment_index)
+    for consciousness_measure in consciousness_measures_data:
+        phase, created = UnConsciousnessMeasurePhase.objects.get_or_create(name=consciousness_measure.phase)
+        main_type, created = UnConsciousnessMeasureType.objects.get_or_create(name=consciousness_measure.type)
+        if consciousness_measure.sub_type:
+            sub_type, created = UnConsciousnessMeasureSubType.objects.get_or_create(name=consciousness_measure.sub_type,
+                                                                                    type=main_type)
+        else:
+            sub_type = None
+        UnConsciousnessMeasure.objects.create(experiment=experiment, phase=phase, type=main_type, sub_type=sub_type,
+                                              number_of_trials=consciousness_measure.number_of_trials,
+                                              number_of_participants_in_awareness_test = consciousness_measure.number_of_awareness_participants,
+                                              is_cm_same_participants_as_task=consciousness_measure.is_cm_pax_same_as_task,
+                                              is_performance_above_chance=consciousness_measure.is_performance_above_chance,
+                                              is_trial_excluded_based_on_measure=consciousness_measure.is_trial_excluded_based_on_measure)
+        if consciousness_measure.notes:
+            experiment.consciousness_measures_notes.add(f"{consciousness_measure.notes}; ")
 
     return experiment

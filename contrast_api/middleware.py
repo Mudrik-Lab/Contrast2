e938e8db8739c1ee7fca10febf8aba0c97b0148f
alonisser <alonisser@gmail.com>
2025-01-04 23:03:20 +0200

    fix middleware

--- contrast_api/middleware.py
+++ contrast_api/middleware.py
@@ -1,7 +1,7 @@
 import os
 from os import getenv
 
-import pytz
+from zoneinfo import ZoneInfo
 from django.utils import timezone
 from spa.middleware import SPAMiddleware
 from django.conf import settings
@@ -14,7 +14,7 @@ class TimezoneMiddleware:
 
     def __call__(self, request):
         environment_timezone = getenv("USER_TZ", "Asia/Jerusalem")
-        activated_timezone = pytz.timezone(environment_timezone)
+        activated_timezone = ZoneInfo(environment_timezone)
         timezone.activate(activated_timezone)
         return self.get_response(request)
 

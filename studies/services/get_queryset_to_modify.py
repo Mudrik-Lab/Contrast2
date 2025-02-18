119080c22c6fc03d0f3b00fa29247eaf820adcc1
dani-efr <69506413+dani-efr@users.noreply.github.com>
2025-02-18 10:26:21 +0200

    Update get_queryset_to_modify.py

--- studies/services/get_queryset_to_modify.py
+++ studies/services/get_queryset_to_modify.py
@@ -2,7 +2,8 @@ from django.db.models import Q
 
 
 def get_queryset_to_modify(experiment_model, safe_list, max_id, first_technique, second_technique):
-    experiments = experiment_model.objects.filter(~Q(id__in=safe_list) & Q(id__lte=max_id))
-    experiments_to_modify = experiments.filter(Q(techniques=first_technique) & Q(techniques=second_technique))
+    experiments_to_modify = experiment_model.objects.filter(
+        ~Q(id__in=safe_list) & Q(id__lte=max_id) & Q(techniques=first_technique) & Q(techniques=second_technique)
+    )
 
     return experiments_to_modify
